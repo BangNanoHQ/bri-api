5c4c4c1a953a2580ebd16d9e2805a17813199f07
Parama Danoesubroto <paramaw@gmail.com>
2025-02-10 23:22:32 -0500

    Added transfer api

---
+++ examples/transfer.rs
@@ -0,0 +1,76 @@
+use bri_api::auth::AuthClient;
+use bri_api::bifast::v2::client::{BifastV2Client, TransferRequest, Amount, AdditionalInfo};
+use chrono::{DateTime, FixedOffset, Utc};
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Load environment variables
+    dotenv::from_filename(".env.dev").ok();
+
+    let base_url = std::env::var("BRI_API_URL")
+        .unwrap_or_else(|_| "https://sandbox.partner.api.bri.co.id".to_string());
+    let client_id = std::env::var("BRI_CLIENT_ID").expect("BRI_CLIENT_ID not set");
+    let private_key = std::env::var("BRI_PRIVATE_KEY").expect("BRI_PRIVATE_KEY not set");
+    let client_secret = std::env::var("BRI_CLIENT_SECRET").expect("BRI_CLIENT_SECRET not set");
+
+    println!("Creating auth client...");
+    let auth_client = AuthClient::new(base_url.clone(), client_id, &private_key)?;
+
+    println!("Creating bifast client...");
+    let bifast_client = BifastV2Client::new(
+        base_url,
+        auth_client,
+        client_secret,
+        "sandbox_partner_id".to_string(), // You might want to move these to env vars
+        "12345".to_string(),
+        "sandbox_external_id".to_string(),
+    );
+
+    let timezone_offset = FixedOffset::east_opt(7 * 3600).unwrap(); // Jakarta Timezone (+07:00)
+    let now_utc: DateTime<Utc> = Utc::now();
+    let now_jakarta: DateTime<FixedOffset> = now_utc.with_timezone(&timezone_offset);
+
+    let transaction_date = now_jakarta.format("%Y-%m-%dT%H:%M:%S%:z").to_string();
+    println!("Transaction Date: {}", transaction_date);
+
+    // Example Transfer Request
+    let transfer_request = TransferRequest {
+        customer_reference: "0000004".to_string(),
+        sender_identity_number: "3515085211930002".to_string(),
+        source_account_no: "001901000613303".to_string(),
+        amount: Amount {
+            value: "23000.00".to_string(),
+            currency: "IDR".to_string(),
+        },
+        beneficiary_bank_code: "CENAIDJA".to_string(),
+        beneficiary_account_no: "2571075557".to_string(),
+        reference_no: "20220929BRINIDJA51050005403".to_string(),
+        external_id: "54445790453".to_string(),
+        transaction_date: transaction_date, // Ensure the timezone is included and correctly formattede timezone is included and correctly formatted
+        payment_info: Some("345345".to_string()),
+        sender_type: "01".to_string(),
+        sender_resident_status: "01".to_string(),
+        sender_town_name: "0300".to_string(),
+        additional_info: Some(AdditionalInfo {
+            device_id: "12345679237".to_string(),
+            channel: "mobilephone".to_string(),
+            is_rdn: "true".to_string(),
+        }),
+    };
+
+
+    println!("Performing Transfer...");
+    let result = bifast_client.transfer(transfer_request).await;
+
+    match result {
+        Ok(transfer_response) => {
+            println!("Transfer Response: {:?}", transfer_response);
+        }
+        Err(err) => {
+            eprintln!("Error during Transfer: {:?}", err);
+        }
+    }
+
+    Ok(())
+}
--- src/bifast/v2/client.rs
+++ src/bifast/v2/client.rs
@@ -88,6 +88,58 @@ impl BifastV2Client {
 
         Ok(response.json::<InquiryResponse>().await?)
     }
+
+    pub async fn transfer(&self, request: TransferRequest) -> Result<TransferResponse, Box<dyn std::error::Error>> {
+        let access_token = self.auth_client.get_access_token().await?.access_token;
+
+        let api_path = "/v2.0/transfer-bifast/payment-bifast";
+        let timestamp = chrono::Utc::now()
+            .format("%Y-%m-%dT%H:%M:%S")
+            .to_string() + &format!(".{:03}Z", chrono::Utc::now().timestamp_subsec_millis());
+
+        // Create request body and calculate body hash
+        let request_body = to_string(&request)?;
+        println!("Request body: {}", request_body);
+        let body_hash = {
+            let mut hasher = Sha256::new();
+            hasher.update(request_body.as_bytes());
+            format!("{:x}", hasher.finalize())
+        };
+
+        // Generate signature
+        let string_to_sign = format!(
+            "POST:{}:{}:{}:{}",
+            api_path, access_token, body_hash, timestamp
+        );
+
+        let signature = {
+            let mut mac = HmacSha512::new_from_slice(self.client_secret.as_bytes())
+                .expect("HMAC can take key of any size");
+            mac.update(string_to_sign.as_bytes());
+            format!("{:x}", mac.finalize().into_bytes())
+        };
+
+        // Make the API request
+        let response = self.client
+            .post(format!("{}{}", self.base_url, api_path))
+            .header("Authorization", format!("Bearer {}", access_token))
+            .header("Content-Type", "application/json")
+            .header("X-TIMESTAMP", &timestamp)
+            .header("X-SIGNATURE", signature)
+            .header("X-PARTNER-ID", &self.partner_id)
+            .header("CHANNEL-ID", &self.channel_id)
+            .header("X-EXTERNAL-ID", &self.external_id)
+            .json(&request)
+            .send()
+            .await?;
+
+        if !response.status().is_success() {
+            let error_text = response.text().await?;
+            return Err(format!("API request failed: {}", error_text).into());
+        }
+
+        Ok(response.json::<TransferResponse>().await?)
+    }
 }
 
 #[cfg(test)]
@@ -158,6 +210,93 @@ mod tests {
         auth_mock.assert();
         inquiry_mock.assert();
     }
+
+    #[tokio::test]
+    async fn test_transfer_success() {
+        let mut server = mockito::Server::new();
+
+        // Mock auth token request
+        let auth_mock = server.mock("POST", "/snap/v1.0/access-token/b2b")
+            .with_status(200)
+            .with_body(r#"{"access_token":"test_token","token_type":"Bearer","expires_in":"899"}"#)
+            .create();
+
+        // Mock transfer request
+        let transfer_mock = server.mock("POST", "/v2.0/transfer-bifast/payment-bifast")
+            .match_header("Authorization", "Bearer test_token")
+            .with_status(200)
+            .with_body(r#"{
+                "responseCode": "2008000",
+                "responseMessage": "Successful",
+                "customerReference": "54321",
+                "sourceAccountNo": "001901000378301",
+                "beneficiaryAccountNo": "12345678900",
+                "beneficiaryBankCode": "CENAIDJA",
+                "referenceNo": "20220128CENAIDJA1698678312816763100",
+                "externalId": "038455145464",
+                "journalSequence": "8292049",
+                "originalReferenceNo": "20220128BRINIDJA794224862497256",
+                "amount": {
+                    "value": "120000.00",
+                    "currency": "IDR"
+                },
+                "additionalInfo": {
+                    "deviceId": "12345679237",
+                    "channel": "mobilephone",
+                    "isRdn": "True"
+                }
+            }"#)
+            .create();
+
+        let auth_client = AuthClient::new(
+            server.url(),
+            "test_client".to_string(),
+            include_str!("../../../tests/test_private_key.pem"),
+        ).unwrap();
+
+        let client = BifastV2Client::new(
+            server.url(),
+            auth_client,
+            "test_secret".to_string(),
+            "test_partner".to_string(),
+            "12345".to_string(),
+            "test_external".to_string(),
+        );
+
+        let request = TransferRequest {
+            customer_reference: "0000004".to_string(),
+            sender_identity_number: "3515085211930002".to_string(),
+            source_account_no: "001901000613303".to_string(),
+            amount: Amount {
+                value: "23000.00".to_string(),
+                currency: "IDR".to_string(),
+            },
+            beneficiary_bank_code: "CENAIDJA".to_string(),
+            beneficiary_account_no: "2571075557".to_string(),
+            reference_no: "20220929BRINIDJA51050005403".to_string(),
+            external_id: "54445790453".to_string(),
+            transaction_date: "2022-11-30T22:25:24+07:00".to_string(),
+            payment_info: Some("345345".to_string()),
+            sender_type: "01".to_string(),
+            sender_resident_status: "01".to_string(),
+            sender_town_name: "0300".to_string(),
+            additional_info: Some(AdditionalInfo {
+                device_id: "12345679237".to_string(),
+                channel: "mobilephone".to_string(),
+                is_rdn: "true".to_string(),
+            }),
+        };
+
+        let result = client.transfer(request).await;
+        assert!(result.is_ok());
+
+        let response = result.unwrap();
+        assert_eq!(response.response_code, "2008000");
+        assert_eq!(response.customer_reference, Some("54321".to_string()));
+
+        auth_mock.assert();
+        transfer_mock.assert();
+    }
 }
 
 #[derive(Debug, Serialize)]
@@ -185,6 +324,60 @@ pub struct InquiryResponse {
     pub receiver_identity_number: String,
     pub receiver_town_name: String,
     pub currency: String,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TransferRequest {
+    pub customer_reference: String,
+    pub sender_identity_number: String,
+    pub source_account_no: String,
+    pub amount: Amount,
+    pub beneficiary_bank_code: String,
+    pub beneficiary_account_no: String,
+    pub reference_no: String,
+    pub external_id: String,
+    pub transaction_date: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub payment_info: Option<String>,
+    pub sender_type: String,
+    pub sender_resident_status: String,
+    pub sender_town_name: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub additional_info: Option<AdditionalInfo>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Amount {
+    pub value: String,
+    pub currency: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AdditionalInfo {
+    pub device_id: String,
+    pub channel: String,
+    #[serde(rename = "isRdn")]
+    pub is_rdn: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TransferResponse {
+    pub response_code: String,
+    pub response_message: String,
+    pub original_reference_no: Option<String>,
+    pub journal_sequence: Option<String>,
+    pub reference_no: Option<String>,
+    pub external_id: Option<String>,
+    pub customer_reference: Option<String>,
+    pub beneficiary_account_no: Option<String>,
+    pub beneficiary_bank_code: Option<String>,
+    pub amount: Option<Amount>,
+    pub source_account_no: Option<String>,
+    pub additional_info: Option<AdditionalInfo>,
 }
 
 
